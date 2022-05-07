134bc908c1049872181ae4737a3280be45239397
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-05-06 23:24:26 -0600

    Add DOM manipulation functions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let library = [];
 let newBook;
+const container = document.querySelector('.container')
 
 class Book {
     constructor (title, author, genre, read) {
@@ -13,5 +14,30 @@ class Book {
 function addBookToLibrary(){
     newBook = new Book(title, author, genre, read);
     library.push(newBook);
-    form.reset()
+    form.reset();
+}
+
+function createCard(book) {
+    const card = document.createElement('div');
+    const title = document.createElement('h2');
+    const author = document.createElement('h3');
+    const genre = document.createElement('span');
+
+    card.classList.add('card');
+    card.setAttribute('id', library.indexOf(book));
+
+    title.textContent = book.title;
+    card.appendChild(title);
+
+    author.textContent = book.author;
+    card.appendChild(author);
+
+    genre.textContent = book.genre;
+    card.appendChild(genre);
+
+    container.appendChild(card);
+}
+
+function resetDisplay() {
+    container.textContent = '';
 }
